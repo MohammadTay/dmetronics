b430187e682159f6ea4c41945a79bdb8ff9ad37a
mite <mohammadh.tay@gmail.com>
2023-11-28 17:36:09 -0500

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,6 +11,8 @@ const Home = () => {
       <Featured/>
       <FeaturedBox/>
       <FeaturedImage/>
+      <img src='https://firebasestorage.googleapis.com/v0/b/phone-dcbd2.appspot.com/o/614px-Bosch_EDC_7_VOLVO_PENTA.jpg?alt=media&token=25c7b5c3-d20e-4235-a9bb-71833d4e1e2e'
+      alt='' />
     </div>
   )
 }
