14887e553b523b76a869aa7b38161daea5cc1ea5
mite <mohammadh.tay@gmail.com>
2023-12-02 12:11:20 -0500

    remove apiext

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "https://dmetronics.onrender.com/:8800/api/",
+    baseURL: "https://dmetronics.onrender.com/api/",
     withCredentials: true,
 });
 
